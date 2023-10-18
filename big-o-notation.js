ade33ec1968fe161fb5fabcd83e056b44fc1e38b
jo-duchan <jo_duchan@icloud.com>
2023-10-19 00:35:17 +0900

    Section 2-5 Big O 소개

---
+++ big-o-notation.js
@@ -0,0 +1,10 @@
+// Section 2 Big O Notation
+
+// Section 2-5
+// https://cs.slides.com/colt_steele/big-o-notation
+
+//Who Cares?
+// - it's important to have a precise vocabulary to talk about how our code performs.
+// - Useful for discussing  trade-offs between different approaches.
+// - When your code slows down or crashes, identifying parts of the code that are inefficient help us find pain points in our applications.
+// - Less important: it comes up in interviews.
