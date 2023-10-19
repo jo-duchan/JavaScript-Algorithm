7119edf1993efaca30706a941e3474a8b9ad4d6b
jo-duchan <jo_duchan@icloud.com>
2023-10-19 23:48:38 +0900

    Section 2-7 연산 갯수 세기

--- big-o-notation.js
+++ big-o-notation.js
@@ -17,13 +17,13 @@
 // - More readable?
 
 // case 01
-function addUpTo(n) {
-  let total = 0;
-  for (let i = 1; i <= n; i++) {
-    total += i;
-  }
-  return total;
-}
+// function addUpTo(n) {
+//   let total = 0;
+//   for (let i = 1; i <= n; i++) {
+//     total += i;
+//   }
+//   return total;
+// }
 
 // case 02
 // function addUpTo(n) {
@@ -31,14 +31,22 @@ function addUpTo(n) {
 // }
 
 //Why not use timers?
-
-let t1 = performance.now();
-addUpTo(1000000000);
-let t2 = performance.now();
-
-console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+// let t1 = performance.now();
+// addUpTo(1000000000);
+// let t2 = performance.now();
+// console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
 
 //The Problem with Time
 // - Different machines will record different times.
 // - The same machine will record different times!
 // - For fast algorithms, speed measurements may not be precise enough?
+
+// Section 2-7
+
+// If not time, then what?
+// Rather than counting seconds, which are so variable...
+// Let's count the number of simple operations the computer has to perform!
+
+// Counting is hard!
+// Depending on what we count, the number of operations can be as low as 2n or as high as 5n + 2
+// But regardless of the exact number, the number of operations grows roughly proportionally with n
