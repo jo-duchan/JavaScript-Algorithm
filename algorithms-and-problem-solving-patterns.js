9e1dcca86eac5c4ce97e1b258e674b9b5d7838f7
jo-duchan <jo_duchan@icloud.com>
2023-11-05 00:36:20 +0900

    Section 4-20. 1단계:문제의 이해

--- algorithms-and-problem-solving-patterns.js
+++ algorithms-and-problem-solving-patterns.js
@@ -22,3 +22,15 @@
 // - Break It Down
 // - Solve/Simplify
 // - Look Back and Refactor
+
+// Section 4-20. 1단계:문제의 이해
+
+// Understand the problem
+// 1. Can I restate the problem in my own words?
+// 2. What are the inputs that go into the problem?
+// 3. What are the outputs that should come from the solution to the problem?
+// 4. Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem?
+// (You may not be able to answer this question until you set about solving the problem. That's okay. it's still worth considering the question at this early stage.)
+// 5. How should I label the important pieces of data that are a part of the problem?
+
+// Write a function which takes two numbers and returns their sum.
