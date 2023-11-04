21408fbda3fa7c109bdcafc1c968dc1db85da847
jo-duchan <jo_duchan@icloud.com>
2023-11-04 23:54:50 +0900

    Section 4-19. 문제 해결법 소개

---
+++ algorithms-and-problem-solving-patterns.js
@@ -0,0 +1,24 @@
+// Section 4-19. 문제 해결법 소개
+
+// Objectoves
+// - Define what an algorithm is
+// - Devise a plan to solve algorithms
+// - Compare and contrast problem solving patterns including frequency counters, two pointer problems and divide and conquer
+
+// What is an algorithm?
+// A process or set of steps to accomplish a certain task.
+
+// What do I need to know this?
+// Almost everything that you do in programming involves some kind of algorithm!
+// It's the foundation for being a successful problem solving and developer Also Interviews
+
+// How do you improve?
+// 1. Devise a plan for solving problems
+// 2. Master common problem solving patterns
+
+// Problem solving
+// - Understand the problem
+// - Explore Concrete Examples
+// - Break It Down
+// - Solve/Simplify
+// - Look Back and Refactor
