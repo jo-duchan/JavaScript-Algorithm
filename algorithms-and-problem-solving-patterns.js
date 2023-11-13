5231fbd3e5b3ad9e6d5d79339a45a9c5f732ba67
jo-duchan <jo_duchan@icloud.com>
2023-11-14 00:10:08 +0900

    Section 4-25.

--- algorithms-and-problem-solving-patterns.js
+++ algorithms-and-problem-solving-patterns.js
@@ -147,12 +147,23 @@
 function charCount(str) {
     var obj = {};
     for (var char of str) {
-        char = char.toLowerCase();
-        if (/[a-z0-9]/.test(char)) {
+        if (isAlphaNumberic(char)) {
+            char = char.toLowerCase();
             obj[char] = ++obj[char] || 1;
         }
     }
     return obj;
+}
+
+// 정규식 대비 55% 이상 빠름
+function isAlphaNumberic(char) {
+    var code = char.charCodeAt(0);
+    if (!(code > 47 && code < 58) && // numeric(0-9)
+        !(code > 64 && code < 91) && // upper alpha(A-Z)
+        !(code > 96 && code < 123)) {
+        return false;
+    }
+    return true;
 }
 
 console.log(charCount("Hi there!"));
