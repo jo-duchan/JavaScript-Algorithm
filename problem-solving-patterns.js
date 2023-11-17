1bfa57bf81a5ed00dba3564ef0e0e3f0ce889a59
jo-duchan <jo_duchan@icloud.com>
2023-11-17 21:52:21 +0900

    Section 5-27. 문제 해결 패턴 소개

---
+++ problem-solving-patterns.js
@@ -0,0 +1,15 @@
+// Section 5-27. 문제 해결 패턴 소개
+
+// How do you improve?
+// 1. Devise a plan for solving problems.
+// 2. Master common problem solving patterns.
+
+// Some patterns...
+// - Frequency Counter
+// - Multiple Pointers
+// - Sliding Window
+// - Divide and Conquer
+// - Dynamic Programming
+// - Greedy Agorithms
+// - Backtracking
+// - Many more!
