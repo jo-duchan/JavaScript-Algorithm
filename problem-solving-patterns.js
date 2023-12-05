d88e968e677ad4c131fe3c6115c192ad4b41c7e7
jo-duchan <jo_duchan@icloud.com>
2023-12-06 03:39:38 +0900

    Section 5-34. 기준점 간 이동 배열 패턴

--- problem-solving-patterns.js
+++ problem-solving-patterns.js
@@ -186,14 +186,68 @@ countUniqueValues([-2, -1, -1, 0, 1]); // 4
 
 // Section 5-33. 고유값 세기 솔루션
 
-function countUniqueValues(arr) {
-  if (arr.length === 0) return 0;
-  let i = 0;
-  for (let j = 1; j < arr.length; j++) {
-    if (arr[i] !== arr[j]) {
-      i++;
-      arr[i] = arr[j];
-    }
+// function countUniqueValues(arr) {
+//   if (arr.length === 0) return 0;
+//   let i = 0;
+//   for (let j = 1; j < arr.length; j++) {
+//     if (arr[i] !== arr[j]) {
+//       i++;
+//       arr[i] = arr[j];
+//     }
+//   }
+//   return i + 1;
+// }
+
+// Section 5-34. 기준점 간 이동 배열 패턴
+
+// Sliding Window
+// This pattern involves creating a window which can either be an array or number from one position to another
+// Depending on a certain condition, the window either increases or closes (and a new window is created)
+// Very useful for keeping track of a subset of data in an array/string etc.
+
+// An Example
+// Write a function called maxSubarraySum which accepts an array of integers and a number called n.
+// The function should calculate the maximum sum of n consecutive elements in the array.
+
+maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2); // 10
+maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4); // 17
+maxSubarraySum([4, 2, 1, 6], 1); // 6
+maxSubarraySum([4, 2, 1, 6, 2], 4); // 13
+maxSubarraySum([], 4); // null
+
+// function maxSubarraySum(arr, num) {
+//   if (num > arr.length) {
+//     return null;
+//   }
+//   var max = -Infinity;
+//   for (let i = 0; i < arr.length - num + 1; i++) {
+//     temp = 0;
+//     for (let j = 0; j < num; j++) {
+//       temp += arr[i + j];
+//     }
+//     if (temp > max) {
+//       max = temp;
+//     }
+//   }
+//   return max;
+// }
+
+// Time Complexity - O(N^2)
+
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
   }
-  return i + 1;
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
 }
+
+//Time Complexity - O(N)
+maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3);
