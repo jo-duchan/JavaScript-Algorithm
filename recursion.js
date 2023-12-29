84060f3aec63e948007fd817a3a0e7f83afea789
jo-duchan <jo_duchan@icloud.com>
2023-12-30 00:07:44 +0900

    Section 7-41. 스토리 타임: 마틴과 드래곤

---
+++ recursion.js
@@ -0,0 +1,9 @@
+// Section 7-41. 스토리 타임: 마틴과 드래곤
+
+// https://cs.slides.com/colt_steele/searching-algorithms-22
+
+// Objectives
+// - Define what recursion is and how it can be used
+// - Understand the two essential components of a recursive function
+// - Visualize the call stack to better debug and understand recursive functions
+// - Use helper method recursion and pure recursion to solve more difficult problems
