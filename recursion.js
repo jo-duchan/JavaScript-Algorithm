604d19d02a484e0be9f39db6dc9b62eb511258c1
jo-duchan <jo_duchan@icloud.com>
2023-12-31 00:55:28 +0900

    Section 7-42. 재귀 함수를 사용하는 이유

--- recursion.js
+++ recursion.js
@@ -7,3 +7,16 @@
 // - Understand the two essential components of a recursive function
 // - Visualize the call stack to better debug and understand recursive functions
 // - Use helper method recursion and pure recursion to solve more difficult problems
+
+// Section 7-42. 재귀 함수를 사용하는 이유
+
+// What is recursion?
+// A process(a function in our case) that calls itself
+
+// Why do I need to know this?
+// It's EVERYWHERE!
+// - JSON.parse / JSON.stringify
+// - document.getElementById and DOM traversal algorithms
+// - Object traversal
+// - We will see it with more complex data structures
+// - It's sometimes a cleaner alternative to iteration
