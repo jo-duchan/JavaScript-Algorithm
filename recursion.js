6331b6c773bdcb42bcb61b0e16a5fc11dff2c0a0
jo-duchan <jo_duchan@icloud.com>
2023-12-31 21:42:18 +0900

    Section 7-43. 스택 호출하기

--- recursion.js
+++ recursion.js
@@ -20,3 +20,18 @@
 // - Object traversal
 // - We will see it with more complex data structures
 // - It's sometimes a cleaner alternative to iteration
+
+// Section 7-43. 스택 호출하기
+
+// But first...let's talk about fuctions
+// - In almost all program languages, there is a built in data structure that manages what happens when functions are invoked
+// - It's named the call stack!
+
+// The call stack
+// - It's a stack data structure - we'll talk about that later!
+// - Any time a funtion is invoked it is placed(pushed) on the top of the call stack
+// - When JavaScript sees the return keyword or when the function ends, the compiler will remove(pop)
+
+// Why do I care?
+// - You're used to function being pushed on the call stack and popped off when they are done
+// - When we write recursive functions, we keep pushing new functions onto the call stack!
