d6616a8eb8a0e1ceaa9ded457b6c132ba3ad2124
jo-duchan <jo_duchan@icloud.com>
2024-02-06 09:53:07 +0900

    Section 9 보너스 도전 재귀 문제 코딩 연습 18 flatten

--- recursion.js
+++ recursion.js
@@ -317,3 +317,24 @@ function someRecursive(arr, callback) {
 // someRecursive([4,6,8,9], isOdd) // true
 // someRecursive([4,6,8], isOdd) // false
 // someRecursive([4,6,8], val => val > 10); // false
+
+// Section 9 보너스 도전 재귀 문제 코딩 연습 18 flatten
+
+function flatten(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      newArr = newArr.concat(flatten(arr[i]));
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+
+  return newArr;
+}
+
+// flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
+// flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+// flatten([[1],[2],[3]]) // [1,2,3]
+// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3
