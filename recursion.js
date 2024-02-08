90f14041e050ff3a2aa54c7e597eff613d1e7e28
jo-duchan <jo_duchan@icloud.com>
2024-02-08 11:02:36 +0900

    Section 9 보너스 도전 재귀 문제 코딩 연습 20 nestedEvenSum

--- recursion.js
+++ recursion.js
@@ -356,3 +356,44 @@ function capitalizeFirst(arr) {
 }
 
 // capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
+
+// Section 9 보너스 도전 재귀 문제 코딩 연습 20 nestedEvenSum
+
+function nestedEvenSum(obj) {
+  let result = 0;
+
+  Object.values(obj).map((value) => {
+    if (typeof value === "number") {
+      result += value % 2 === 0 ? value : 0;
+    }
+
+    if (typeof value === "object") {
+      result += nestedEvenSum(value);
+    }
+  });
+
+  return result;
+}
+
+var obj1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup",
+    },
+  },
+};
+
+var obj2 = {
+  a: 2,
+  b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+  c: { c: { c: 2 }, cc: "ball", ccc: 5 },
+  d: 1,
+  e: { e: { e: 2 }, ee: "car" },
+};
+
+// nestedEvenSum(obj1); // 6
+// nestedEvenSum(obj2); // 10
